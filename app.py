127b981e55c50e2de25bb5e3cf17853b5adba965
julianvitor <julian.vtr@gmail.com>
2023-10-03 15:07:57 -0300

    suprimir saida do exifread

--- app.py
+++ app.py
@@ -1,89 +1,106 @@
-import exifread,folium,json,os,time,coord_converter
+import exifread, folium, json, os, time, coord_converter, sys
 from tqdm import tqdm
 from functools import cache
 
 # Função para extrair informações de localização de uma imagem
 @cache
-def extract_location_info(file_path):
-    with open(file_path, 'rb') as image_file:
-        tags = exifread.process_file(image_file)
+def extrair_informacoes_localizacao(caminho_arquivo):
+    with open(caminho_arquivo, 'rb') as arquivo_imagem:
+        # Redireciona a saída padrão (stdout) e a saída de erro (stderr) para um objeto de captura
+        saida_padrao_original = sys.stdout
+        saida_erro_original = sys.stderr
+        sys.stdout = open(os.devnull, 'w')
+        sys.stderr = open(os.devnull, 'w')
+        try:
+            tags = exifread.process_file(arquivo_imagem)
+        finally:
+            # Restaura a saída padrão e a saída de erro
+            sys.stdout.close()
+            sys.stderr.close()
+            sys.stdout = saida_padrao_original
+            sys.stderr = saida_erro_original
 
     if 'GPS GPSLatitude' in tags and 'GPS GPSLongitude' in tags:
-        latitude_values = tags['GPS GPSLatitude'].values
-        longitude_values = tags['GPS GPSLongitude'].values
+        valores_latitude = tags['GPS GPSLatitude'].values
+        valores_longitude = tags['GPS GPSLongitude'].values
 
         # Converta latitude e longitude usando o módulo em C
-        latitude = coord_converter.convert_coordinates(tuple(float(val) for val in latitude_values))
-        longitude = coord_converter.convert_coordinates(tuple(float(val) for val in longitude_values))
+        latitude = coord_converter.convert_coordinates(tuple(float(val) for val in valores_latitude))
+        longitude = coord_converter.convert_coordinates(tuple(float(val) for val in valores_longitude))
 
-        latitude_ref = tags['GPS GPSLatitudeRef'].values
-        longitude_ref = tags['GPS GPSLongitudeRef'].values
+        referencia_latitude = tags['GPS GPSLatitudeRef'].values
+        referencia_longitude = tags['GPS GPSLongitudeRef'].values
 
-        latitude, longitude = adjust_coordinates(latitude, longitude, latitude_ref, longitude_ref)
+        latitude, longitude = ajustar_coordenadas(latitude, longitude, referencia_latitude, referencia_longitude)
 
         return latitude, longitude
     else:
         return None
 
 # Função para ajustar as coordenadas para valores negativos, se necessário
-def adjust_coordinates(latitude, longitude, latitude_ref, longitude_ref):
+def ajustar_coordenadas(latitude, longitude, referencia_latitude, referencia_longitude):
     # Verifique se latitude e longitude precisam ser negadas com base nos valores de referência
-    if latitude_ref == 'S':
+    if referencia_latitude == 'S':
         latitude = -latitude
-    if longitude_ref == 'W':
+    if referencia_longitude == 'W':
         longitude = -longitude
 
     return latitude, longitude
 
 # Função para criar um mapa com marcadores
-def create_map(location_data, map_type):
-    if map_type == "satellite":
+def criar_mapa(dados_localizacao, tipo_mapa):
+    if tipo_mapa == "satelite":
         m = folium.Map(location=[0, 0], zoom_start=2,tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', attr='Esri')
-        for image_path, location_info in location_data.items():
-            latitude = location_info['latitude']
-            longitude = location_info['longitude']
-            folium.Marker((latitude, longitude), popup=image_path).add_to(m)
-        m.save(f'map_{map_type}.html')#save map
-    elif map_type == "standard":
+        for caminho_arquivo, informacao_localizacao in dados_localizacao.items():
+            latitude = informacao_localizacao['latitude']
+            longitude = informacao_localizacao['longitude']
+            folium.Marker((latitude, longitude), popup=caminho_arquivo).add_to(m)
+        m.save(f'mapa_{tipo_mapa}.html') # salvar mapa
+    elif tipo_mapa == "padrao":
         m = folium.Map(location=[0, 0], zoom_start=2)
-        for image_path, location_info in location_data.items():
-            latitude = location_info['latitude']
-            longitude = location_info['longitude']
-            folium.Marker((latitude, longitude), popup=image_path).add_to(m)
-        m.save(f'map_{map_type}.html')
-    
+        for caminho_arquivo, informacao_localizacao in dados_localizacao.items():
+            latitude = informacao_localizacao['latitude']
+            longitude = informacao_localizacao['longitude']
+            folium.Marker((latitude, longitude), popup=caminho_arquivo).add_to(m)
+        m.save(f'mapa_{tipo_mapa}.html')
 
 # Função principal
 def main():
-    start_time = time.time()
+    tempo_inicio = time.time()
 
     # Pasta contendo as imagens
-    image_folder = 'images'
+    pasta_imagens = 'images'
 
-    # Lista para armazenar os caminhos das imagens
-    image_paths = [os.path.join(image_folder, filename) for filename in os.listdir(image_folder) if filename.endswith(('.jpg', '.jpeg'))]
-
+    # Lista para armazenar os caminhos das imagens, excluindo arquivos não suportados
+    extensoes_suportadas = ('.jpg', '.jpeg')
+    caminhos_imagens = [
+        os.path.join(pasta_imagens, nome_arquivo)
+        for nome_arquivo in os.listdir(pasta_imagens)
+        if nome_arquivo.lower().endswith(extensoes_suportadas)
+    ]
     # Dicionário para armazenar os dados de localização
-    location_data = {}
+    dados_localizacao = {}
 
-    # Use tqdm para criar uma barra de progresso durante a extração de dados
-    for image_path in tqdm(image_paths, desc='Extraindo dados de localização'):
-        location_info = extract_location_info(image_path)
-        if location_info:
-            latitude, longitude = location_info
-            location_data[image_path] = {'latitude': latitude, 'longitude': longitude}
-
+    # Use tqdm para criar uma única barra de progresso que abranja todo o processo
+    with tqdm(total=len(caminhos_imagens), desc='Processando imagens') as barra_progresso:
+        for caminho_arquivo in caminhos_imagens:
+            informacao_localizacao = extrair_informacoes_localizacao(caminho_arquivo)
+            if informacao_localizacao:
+                latitude, longitude = informacao_localizacao
+                dados_localizacao[caminho_arquivo] = {'latitude': latitude, 'longitude': longitude}
+            barra_progresso.update(1)  # Atualize a barra de progresso a cada imagem processada
+            
     # Salve os dados em um arquivo JSON
-    with open('location_data.json', 'w') as json_file:
-        json.dump(location_data, json_file, indent=4)
+    with open('dados_localizacao.json', 'w') as arquivo_json:
+        json.dump(dados_localizacao, arquivo_json, indent=4)
 
     # Crie mapas
-    create_map(location_data, "standard")
-    create_map(location_data, "satellite")
+    criar_mapa(dados_localizacao, "padrao")
+    criar_mapa(dados_localizacao, "satelite")
 
-    end_time = time.time()
-    execution_time = end_time - start_time
-    print(f"Tempo de execução: {execution_time:.2f} segundos")
+    tempo_fim = time.time()
+    tempo_execucao = tempo_fim - tempo_inicio
+    print(f"Tempo de execução: {tempo_execucao:.2f} segundos")
 
 if __name__ == "__main__":
     main()
